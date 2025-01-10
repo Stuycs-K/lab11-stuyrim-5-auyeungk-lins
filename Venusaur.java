d635af8556549b38e694a0d109d3f129c4f7dd9b
Selina Lin <selinalin2178@gmail.com>
2025-01-10 02:02:05 -0500

    created file for venusaur

---
+++ Venusaur.java
@@ -0,0 +1,44 @@
+
+public class Venusaur extends Adventurer{
+
+	public String getSpecialName() {
+		return null;
+	}
+
+	public int getSpecial() {
+		
+		return 0;
+	}
+
+
+	public int getSpecialMax() {
+	
+		return 0;
+	}
+
+	public void setSpecial(int n) {
+
+		
+	}
+
+	public String attack(Adventurer other) {
+
+		return null;
+	}
+
+	public String support(Adventurer other) {
+
+		return null;
+	}
+
+	public String support() {
+
+		return null;
+	}
+
+	public String specialAttack(Adventurer other) {
+
+		return null;
+	}
+
+}

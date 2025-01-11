a37802fbb783cf8b10161c70ad80eb4c27c4899c
Selina Lin <selinalin2178@gmail.com>
2025-01-11 04:38:58 -0500

    added more to Charizard's attacks

--- Charizard.java
+++ Charizard.java
@@ -54,7 +54,11 @@ public class Charizard extends Adventurer{
 			baseDmg*=2;
 			setSD(getSD() - 1);
 		}
-		 
+		 if(baseDmg != 0) {
+			 if(Math.random() > 0.75) {
+				 other.setBurnStatus(true);
+			 }
+		 }
 		if (other.getType().equals("Grass")){
 		      other.applyDamage(baseDmg * 2);
 		      if(getSpecial() != getSpecialMax()) {
@@ -78,70 +82,88 @@ public class Charizard extends Adventurer{
 
 	public String support(Adventurer other) {
 		// flamethrower
-		int baseDmg = 4;
-		if(other.getProtect() == true){
-			baseDmg = 0;
-			other.setProtectStatus(false);
+		if (getSpecial()-5 > 0){
+			int baseDmg = 4;
+			if(other.getProtect() == true){
+				baseDmg = 0;
+				other.setProtectStatus(false);
+			}
+			if(this.getHH() == true){
+				baseDmg*=2;
+				this.setHelpingHandStatus(false);
+			}
+			if(this.getSD() > 0) {
+				baseDmg*=2;
+				setSD(getSD() - 1);
+			}
+			setSpecial(getSpecial() - 5);
+			if(baseDmg != 0) {
+				 if(Math.random() > 0.75) {
+					 other.setBurnStatus(true);
+				 }
+			 }
+			if (other.getType().equals("Grass")){
+			      other.applyDamage(baseDmg * 2);
+			      
+			      return this.toString() + " used Flamethrower! It's super effective! It did "+ baseDmg*2+ " dmg!";
+			    }
+			    if (other.getType().equals("Water")){
+			      other.applyDamage(baseDmg/2);
+			      
+			      return this.toString() + " used Flamethrower! It's not very effective... It did " + baseDmg/2 + " dmg...";
+			    }
+					other.applyDamage(baseDmg);
+					
+					return this.toString() + " used Flamethrower! It did " + baseDmg + " dmg!";
+		}else {
+			return "Not enough PP!";
 		}
-		if(this.getHH() == true){
-			baseDmg*=2;
-			this.setHelpingHandStatus(false);
-		}
-		if(this.getSD() > 0) {
-			baseDmg*=2;
-			setSD(getSD() - 1);
-		}
-		
-		if (other.getType().equals("Grass")){
-		      other.applyDamage(baseDmg * 2);
-		      
-		      return this.toString() + " used Flamethrower! It's super effective! It did "+ baseDmg*2+ " dmg!";
-		    }
-		    if (other.getType().equals("Water")){
-		      other.applyDamage(baseDmg/2);
-		      
-		      return this.toString() + " used Flamethrower! It's not very effective... It did " + baseDmg/2 + " dmg...";
-		    }
-				other.applyDamage(baseDmg);
-				
-				return this.toString() + " used Flamethrower! It did " + baseDmg + " dmg!";
 	}
 
 
 	public String support() {
-		//swords dance
-		return null;
+		this.setSD(2);
+		return this.toString() + " used Swords Dance! It powered up its moves for the next 2 turns!";
 	}
 
 	public String specialAttack(Adventurer other) {
 		// flare blitz
-		int baseDmg = 6;
-		if(other.getProtect() == true){
-			baseDmg = 0;
-			other.setProtectStatus(false);
+		if (getSpecial()-10 > 0){
+			int baseDmg = 6;
+			if(other.getProtect() == true){
+				baseDmg = 0;
+				other.setProtectStatus(false);
+			}
+			if(this.getHH() == true){
+				baseDmg*=2;
+				this.setHelpingHandStatus(false);
+			}
+			if(this.getSD() > 0) {
+				baseDmg*=2;
+				setSD(getSD() - 1);
+			}
+			setSpecial(getSpecial() - 10);
+			if(baseDmg != 0) {
+				 if(Math.random() > 0.75) {
+					 other.setBurnStatus(true);
+				 }
+			 }
+			if (other.getType().equals("Grass")){
+			      other.applyDamage(baseDmg * 2);
+			      this.applyDamage(3);
+			      return this.toString() + " used Flare Blitz! It's super effective! It did "+ baseDmg*2 +" dmg! " + this.toString() + " was hit by recoil!";
+			    }
+			    if (other.getType().equals("Water")){
+			      other.applyDamage(baseDmg/2);
+			      this.applyDamage(3);
+			      return this.toString() + " used Flare Blitz! It's not very effective... It did "+ baseDmg/2 +" dmg... " + this.toString() + " was hit by recoil!";
+			    }
+					other.applyDamage(baseDmg);
+					this.applyDamage(3);
+					return this.toString() + " used Flare Blitz! It did " + baseDmg + " dmg! " + this.toString() + " was hit by recoil!";
+		}else {
+			return "Not enough PP!";
 		}
-		if(this.getHH() == true){
-			baseDmg*=2;
-			this.setHelpingHandStatus(false);
-		}
-		if(this.getSD() > 0) {
-			baseDmg*=2;
-			setSD(getSD() - 1);
-		}
-		
-		if (other.getType().equals("Grass")){
-		      other.applyDamage(baseDmg * 2);
-		      
-		      return this.toString() + " used Flare Blitz! It's super effective! It did "+ baseDmg*2 +" dmg!";
-		    }
-		    if (other.getType().equals("Water")){
-		      other.applyDamage(baseDmg/2);
-		     
-		      return this.toString() + " used Flare Blitz! It's not very effective... It did "+ baseDmg/2 +" dmg...";
-		    }
-				other.applyDamage(baseDmg);
-				
-				return this.toString() + " used Flare Blitz! It did " + baseDmg + " dmg!";
 	}
 	
 }
